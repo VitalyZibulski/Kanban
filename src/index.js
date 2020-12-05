45a8a10572739a27fe5ab46a833e5162ebd9b455
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2020-12-05 20:44:21 +0300

    add Progress component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import Button from "./components/Button/Button";
 import ButtonsGroup from "./components/ButtonsGroup/ButtonsGroup";
+import Progress from "./components/Progress/Progress";
 
 const App = () =>{
+  const [percent, setPercent] = useState(0)
+
+  const interval = setInterval(() => {
+    if (percent >= 100) {
+      setPercent(100);
+      clearInterval(interval);
+    } else {
+      setPercent(percent + 20);
+    }
+
+    clearInterval(interval);
+  }, 1000);
+
   return <div>
     {/*<Button>Hello1</Button>*/}
     {/*<Button mode="secondary" disabled>Hello2</Button>*/}
@@ -11,11 +25,13 @@ const App = () =>{
     {/*<Button mode="danger" size="large">Hello4</Button>*/}
     {/*<Button counter={0}>Counter</Button>*/}
 
-    <ButtonsGroup>
-      <Button>Left part</Button>
-      <Button>Middle part</Button>
-      <Button>Right part</Button>
-    </ButtonsGroup>
+    {/*<ButtonsGroup>*/}
+    {/*  <Button>Left part</Button>*/}
+    {/*  <Button>Middle part</Button>*/}
+    {/*  <Button>Right part</Button>*/}
+    {/*</ButtonsGroup>*/}
+
+    <Progress percent={percent}/>
   </div>;
 }
 
