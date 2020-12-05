4bae45f311c31bb96a165bb73ed562dde93fec99
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2020-12-05 03:54:37 +0300

    add component Button with classnames

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Button from "./components/Button/Button";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () =>{
+  return <div>
+    <Button>Hello1</Button>
+    <Button mode="secondary" disabled>Hello2</Button>
+    <Button mode="success" size="small">Hello3</Button>
+    <Button mode="danger" size="large">Hello4</Button>
+  </div>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
