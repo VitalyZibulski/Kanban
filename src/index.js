77a8b1d6ef8a5e9c0cc2f3297bb06648d956772b
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2020-12-08 02:25:50 +0300

    initial project with firebase

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import "core-js/features/map";
+import "core-js/features/set";
+import React from "react";
+import ReactDOM from "react-dom";
+import bridge from "@vkontakte/vk-bridge";
+import App from "./App";
+import firebase from "firebase/app";
+
+import "firebase/auth";
+import "firebase/firestore";
+import 'firebase/analytics'
+
+var firebaseConfig = {
+  apiKey: "AIzaSyASEnsAwJEVyja5okVqzeREhRtVnDWPaX0",
+  authDomain: "kanban-42dc8.firebaseapp.com",
+  projectId: "kanban-42dc8",
+  storageBucket: "kanban-42dc8.appspot.com",
+  messagingSenderId: "907029208239",
+  appId: "1:907029208239:web:6162268fc32be93e24485a",
+  measurementId: "G-42P9KFRKHQ"
+};
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+// Init VK  Mini App
+bridge.send("VKWebAppInit");
+
+const db = firebase.firestore();
+db.collection("desks").get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+    console.log(doc.data());
+  });
+});
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
+if (process.env.NODE_ENV === "development") {
+  import("./eruda").then(({ default: eruda }) => {}); //runtime download
+}
