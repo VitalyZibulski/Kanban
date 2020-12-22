0a17989553d0d0b6521fa4e58ed71a3ab64c8499
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2020-12-22 14:23:39 +0300

    add state to redux

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,5 +1,18 @@
-import { ADD_COLUMN, REMOVE_COLUMN, SET_COLUMNS } from './types';
+import * as actionType from './types';
 
-export const addColumn = (column) => ({ type: ADD_COLUMN, payload: { column }});
-export const removeColumn = (removeId) => ({ type: REMOVE_COLUMN, payload: { removeId }});
-export const setColumns = (columns) => ({ type: SET_COLUMNS, payload: { columns }});
+export const addColumn = (column) => ({ type: actionType.ADD_COLUMN, payload: { column }});
+export const removeColumn = (removeId) => ({ type: actionType.REMOVE_COLUMN, payload: { removeId }});
+export const setColumns = (columns) => ({ type: actionType.SET_COLUMNS, payload: { columns }});
+
+export const addDesk = (desk) => ({ type: actionType.ADD_DESK, payload: { desk }});
+export const removeDesk = (removeId) => ({ type: actionType.REMOVE_DESK, payload: { removeId }});
+export const setDesks = (desks) => ({ type: actionType.SET_DESKS, payload: { desks }});
+
+export const setActivePanel = (panel) => ({ type: actionType.SET_ACTIVE_PANEL, payload: { panel }});
+export const changeRoute = ({ route }) => setActivePanel(route.name);
+
+export const addCard = (card) => ({ type: actionType.ADD_DESK, payload: { card }});
+export const removeCard = (removeId) => ({ type: actionType.REMOVE_DESK, payload: { removeId }});
+export const setCards = (cards) => ({ type: actionType.SET_DESKS, payload: { cards }});
+
+export const setPopout = (popout) => ({ type: actionType.SET_POPOUT, payload: { popout }});
--- src/components/App/hooks.js
+++ src/components/App/hooks.js
@@ -1,40 +1,6 @@
 import { useState, useEffect } from "react";
 import { getDesks } from "../../actions";
 
-const useDesksState = () => {
-  const [desks, setDesks] = useState([]);
-  const addDesk = (desk) => setDesks([...desks, desk]);
-  const removeDesk = (removeId) => setDesks(desks.filter(({id}) => id !== removeId));
-
-  useEffect(() => {
-    // getDesks().then(desks => onLoadDesks(desks))
-    getDesks().then(setDesks)
-  }, []);
-
-  return { desks, addDesk, removeDesk, setDesks };
-}
-
-const useNavState = () => {
-  const [activePanel, setActivePanel] = useState(null);
-  const changeRoute = ({ route }) => setActivePanel(route.name);
-
-  return { activePanel, changeRoute }
-}
-
-const useCardsState = () => {
-  const [cards, setCards] = useState([]);
-  const addCard = (card) => setCards([...cards, card]);
-  const removeCard = (removeId) => setCards(cards.filter(({id}) => id !== removeId));
-
-  return { cards, setCards, addCard, removeCard }
-}
-
-const usePopoutSatate = () => {
-  const [popout, setPopout] = useState(null);
-
-  return { popout, setPopout };
-}
-
 export const useAppState = () =>  {
   const desksState = useDesksState();
   const columnsState = useColumnsState();
