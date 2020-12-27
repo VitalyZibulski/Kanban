d365b87d97374d3f45ea499faee19e9cd43c09da
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2020-12-28 02:42:49 +0300

    add store

--- src/index.js
+++ src/index.js
@@ -2,21 +2,18 @@ import "core-js/features/map";
 import "core-js/features/set";
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
+import { getStore } from "./store";
 
 // import bridge from "@vkontakte/vk-bridge";
 import * as backend from './api';
 import * as router from './router';
 import App from "./components/App/AppContainer";
-import { reducer } from "./reducers/reducer";
 
 // Init VK  Mini App
 // bridge.send("VKWebAppInit");
 const route = router.initialize();
 backend.initialize();
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = getStore();
 
 ReactDOM.render(<App router={route} store={store}/>, document.getElementById("root"));
