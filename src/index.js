0193017318d6896e26e6c15bf59622bf436ee43d
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2021-01-22 01:05:48 +0300

    fix deploy

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import "core-js/features/set";
 import React from "react";
 import ReactDOM from "react-dom";
 import { getStore } from "./app/store";
-
-// import bridge from "@vkontakte/vk-bridge";
+import vkBridge from "@vkontakte/vk-bridge";
 import * as backend from './api';
 import * as router from './router';
 import App from "./app/components/App/AppContainer";
@@ -18,7 +17,8 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Init VK  Mini App
-// bridge.send("VKWebAppInit");
+vkBridge.send("VKWebAppInit");
+
 const route = router.initialize();
 backend.initialize();
 
